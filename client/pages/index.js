f49f45d46e3b634a43434aacf05a8bc09e042d50
Lachitha <it21019586@my.sliit.lk>
2024-04-23 12:34:06 +0530

    update

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,15 +2,15 @@ import axios from "axios";
 import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
-  return <> {currentUser ? <h1>You are sign in</h1> : <h1>No user</h1>}</>;
+	return <> {currentUser ? <h1>You are sign in</h1> : <h1>No user</h1>}</>;
 };
 
 LandingPage.getInitialProps = async (context) => {
-  console.log("Landing page");
-  const client = buildClient(context);
-  const { data } = await client.get("/api/users/currentuser");
+	console.log("Landing page");
+	const client = buildClient(context);
+	const { data } = await client.get("api/users/currentuser");
 
-  return data;
+	return data;
 };
 
 export default LandingPage;
