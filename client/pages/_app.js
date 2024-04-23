4906d608a8280103707fbf5521992e969f24603f
Lachitha <it21019586@my.sliit.lk>
2024-04-23 12:43:04 +0530

    update

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -4,28 +4,28 @@ import Header from "../component/header";
 
 //our other component load throgh this app component
 const AppComponent = ({ Component, pageProps, currentUser }) => {
-  return (
-    <div>
-      <Header currentUser={currentUser} />
-      <Component {...pageProps} />
-    </div>
-  );
+	return (
+		<div>
+			<Header currentUser={currentUser} />
+			<Component {...pageProps} />
+		</div>
+	);
 };
 //If we add getInitialProps to app component , other component initialProps function won't be excute automatiically
 AppComponent.getInitialProps = async (appContext) => {
-  const client = buildClient(appContext.ctx);
-  const { data } = await client.get("/api/users/currentuser");
+	const client = buildClient(appContext.ctx);
+	const { data } = await client.get("api/users/currentuser");
 
-  let pageProps = {};
+	let pageProps = {};
 
-  if (appContext.Component.getInitialProps) {
-    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
-  }
+	if (appContext.Component.getInitialProps) {
+		pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+	}
 
-  return {
-    pageProps,
-    currentUser: data.currentUser,
-  };
+	return {
+		pageProps,
+		currentUser: data.currentUser,
+	};
 };
 
 export default AppComponent;
