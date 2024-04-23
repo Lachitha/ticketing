9c460d9394af29ba99949f43a481af8fa14e84fc
Lachitha <it21019586@my.sliit.lk>
2024-04-23 16:25:46 +0530

    up

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,12 +4,13 @@ import { app } from "./app";
 const start = async () => {
   console.log('JWT_KEY:', process.env.JWT_KEY);
   if (!process.env.JWT_KEY) {
-    throw new Error("JWT_KEY must be defind");
+    throw new Error("JWT_KEY must be defined");
   }
   try {
     await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("MongoDB connected successfully!");
   } catch (err) {
-    console.error(err);
+    console.error("Error connecting to MongoDB:", err);
   }
 
   app.listen(3000, () => {
